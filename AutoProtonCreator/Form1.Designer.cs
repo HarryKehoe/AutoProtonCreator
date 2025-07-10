1fabacaedca768fa2ba9c75faf9ed51cb22c99d3
Harry Kehoe <40206306@live.napier.ac.uk>
2025-07-10 23:59:02 +0100

    Add project files.

---
+++ AutoProtonCreator/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AutoProtonCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(261, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(146, 60);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 73);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "To create an account simply press start. Wait for the captcha to display and solv" +
+    "e it.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 84);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Auto Proton Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AutoProtonCreator/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutoProtonCreator
+{
+    public partial class Form1 : Form
+    {
+        private IWebDriver driver;
+        private static List<string> wordList = new List<string>();
+        private string lastGeneratedPassword = "";
+
+        private const int WM_HOTKEY = 0x0312;
+        private const int HOTKEY_ID = 9000;
+
+        private const uint MOD_CONTROL = 0x0002;
+        private const uint MOD_ALT = 0x0001;
+        private const uint VK_Z = 0x5A; // 'Z'
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Register Ctrl + Alt + Z hotkey
+            RegisterHotKey(this.Handle, HOTKEY_ID, MOD_CONTROL | MOD_ALT, VK_Z);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    if (this.WindowState == FormWindowState.Minimized)
+                        this.WindowState = FormWindowState.Normal;
+
+                    this.Activate();
+                    this.BringToFront();
+                    this.TopMost = true;  // briefly bring on top
+                    this.TopMost = false;
+                    this.Focus();
+                }));
+            }
+            base.WndProc(ref m);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+            base.OnFormClosing(e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadWordsFromFile("words.txt");
+
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+
+            // Get path to the folder where your .exe (and chromedriver.exe) will be
+            string driverPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Create ChromeDriverService to point to that path
+            var driverService = ChromeDriverService.CreateDefaultService(driverPath);
+
+            // Now create ChromeDriver with that service and options
+            driver = new ChromeDriver(driverService, options);
+        }
+
+        private static void LoadWordsFromFile(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    wordList = File.ReadAllLines(path)
+                        .Select(w => w.Trim().ToLower())
+                        .Where(w => w.Length >= 8)
+                        .ToList();
+                }
+                else
+                {
+                    MessageBox.Show("Word file not found: " + path);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading word file: " + ex.Message);
+            }
+        }
+
+        private static string GenerateUsername()
+        {
+            var rand = new Random();
+            if (wordList.Count < 2)
+                return "user." + GetShortTimestamp();
+
+            string word1 = wordList[rand.Next(wordList.Count)];
+            string timestamp = GetShortTimestamp();
+            return $"{word1}.{timestamp}";
+        }
+
+        private static string GeneratePassword()
+        {
+            var rand = new Random();
+            if (wordList.Count < 1)
+                return "Password123!";
+
+            string word1 = wordList[rand.Next(wordList.Count)];
+            string word2 = wordList[rand.Next(wordList.Count)];
+
+            while (word2 == word1 && wordList.Count > 1)
+                word2 = wordList[rand.Next(wordList.Count)];
+
+            int number = rand.Next(10, 100);
+            char[] symbols = { '!', '@', '#', '$', '%', '&' };
+            char symbol = symbols[rand.Next(symbols.Length)];
+
+            return $"{word1}{word2}{number}{symbol}";
+        }
+
+        private static string GetShortTimestamp()
+        {
+            long unixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            byte[] bytes = BitConverter.GetBytes(unixTime);
+            string base64 = Convert.ToBase64String(bytes)
+                .TrimEnd('=')
+                .Replace('+', 'u')
+                .Replace('/', 'x');
+            return base64.Substring(0, 4);
+        }
+
+        private void SaveCredentials(string username, string password)
+        {
+            string line = $"{username}:{password}";
+            File.AppendAllText("accounts.txt", line + Environment.NewLine);
+        }
+
+        private IWebElement WaitForElement(string cssSelector, int timeoutSeconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
+            return wait.Until(d =>
+            {
+                try
+                {
+                    var el = d.FindElement(By.CssSelector(cssSelector));
+                    return (el.Displayed && el.Enabled) ? el : null;
+                }
+                catch (NoSuchElementException)
+                {
+                    return null;
+                }
+            });
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            CreateAccount();
+        }
+
+        private void CreateAccount()
+        {
+            try
+            {
+                driver.Navigate().GoToUrl("https://account.proton.me/mail/signup");
+
+                string username = GenerateUsername();
+                lastGeneratedPassword = GeneratePassword();
+
+                var freePlanButton = WaitForElement("div.plan-card-selector-container button");
+                freePlanButton?.Click();
+
+                var iframe = WaitForElement("form iframe");
+                iframe?.Click();
+
+                var actions = new OpenQA.Selenium.Interactions.Actions(driver);
+                actions.SendKeys(username).Perform();
+
+                var passwordBox = WaitForElement("body > div.app-root > div.flex.\\*\\:min-size-auto.flex-nowrap.flex-column.h-full.overflow-auto.relative.signup-v2-wrapper.signup-v2-bg.signup-v2-bg--mail > div > div:nth-child(1) > div.single-box.relative.mt-12.w-full.max-w-custom > div.pricing-box-content.mt-8 > div > div.flex-1.w-0.relative > form > div.flex.flex-column.mb-4 > div.field-two-container.field-two--dense.mt-2.pb-2 > div.field-two-input-container.relative");
+                passwordBox?.Click();
+                actions.SendKeys(lastGeneratedPassword).Perform();
+
+                var confirmBox = WaitForElement("form div:nth-child(4) div.field-two-input-container");
+                confirmBox?.Click();
+                actions.SendKeys(lastGeneratedPassword).Perform();
+
+                var startButton = WaitForElement("form > div:nth-child(3) > button");
+                startButton?.Click();
+
+                var xButton = WaitForElement("body > div.modal-two > dialog > div > div.flex.mb-4.items-center.justify-center > div:nth-child(3) > button");
+                xButton?.Click();
+
+                try
+                {
+                    var continueButton = WaitForElement("form button[type='submit']", 240);
+                    continueButton?.Click();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Continue button not found or clickable: " + ex.Message);
+                    return;
+                }
+
+                try
+                {
+                    var maybeLaterButton = WaitForElement("form button.button-ghost-norm", 120);
+                    maybeLaterButton?.Click();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Maybe later button not found: " + ex.Message);
+                }
+
+                try
+                {
+                    var confirmButton = WaitForElement("dialog button.button-solid-norm", 120);
+                    confirmButton?.Click();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Confirm button not found: " + ex.Message);
+                }
+
+                try
+                {
+                    // 1. Click "Let's get started"
+                    var letsGetStarted = WaitForElement("#modal-340-description > footer > button", 120);
+                    letsGetStarted?.Click();
+                    System.Threading.Thread.Sleep(500);
+
+                    // 2. Click "Maybe later"
+                    var maybeLater = WaitForElement("#modal-340-description > footer > button", 120);
+                    maybeLater?.Click();
+                    System.Threading.Thread.Sleep(500);
+
+                    // 3. Click "Next"
+                    var nextButton = WaitForElement("#modal-340-description > div:nth-child(1) > div:nth-child(2) > footer > button", 120);
+                    nextButton?.Click();
+                    System.Threading.Thread.Sleep(500);
+
+                    // 4. Click "Use this"
+                    var useThis = WaitForElement("#modal-340-description > footer > button", 120);
+                    useThis?.Click();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during final onboarding steps: " + ex.Message);
+                }
+
+                SaveCredentials(username, lastGeneratedPassword);
+
+                //Logout
+                //Click profile menu
+                var profileMenu = WaitForElement("body > div.app-root > div.flex.flex-row.flex-nowrap.h-full > div > div > div > div.flex.flex-column.flex-1.flex-nowrap.reset4print > div > div > header > div.topnav-container.flex.flex-none.md\\:flex-initial.justify-end.self-center.my-auto.ml-auto.md\\:ml-0.no-print > ul > li.topnav-listItem.relative.hidden.md\\:flex > button");
+                profileMenu?.Click();
+
+                //Click logout
+                var logoutButton = WaitForElement("div.dropdown-content > div > div.mb-4.px-4.flex.flex-column.gap-2 > button");
+                logoutButton?.Click();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating account: " + ex.Message);
+            }
+        }
+    }
+}
